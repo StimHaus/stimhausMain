f3cd58db2b0ad036e694a16ac97a41dfd752731b
Nicholas Hazel <sinistersystemmgr@gmail.com>
2017-02-04 06:45:54 -0800

    Updated navigation

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation();
+
+var hamburger = $('#menu-toggle');
+var offCanvasRight = $("#offCanvasRight");
+var overlay = $('.js-off-canvas-overlay');
+hamburger.click(function(){
+  $(this).addClass('open');
+});
+overlay.click(function(){
+	hamburger.removeClass('open');
+});
