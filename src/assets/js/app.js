92a0ea95d87be7ce5f6284052c31007e3ecaeb6b
Nicholas Hazel <sinistersystemmgr@gmail.com>
2017-02-04 06:59:32 -0800

    Merging nav branch with master branch

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,11 @@
 $(document).foundation();
 
+// DEFINE GLOBAL VARIABLES FOR COMMON OBJECTS //
 var hamburger = $('#menu-toggle');
 var offCanvasRight = $("#offCanvasRight");
 var overlay = $('.js-off-canvas-overlay');
+
+// CREATE OFF CANVAS NAVIGATION TOGGLE //
 hamburger.click(function(){
   $(this).addClass('open');
 });
